25aa328a27917dcc51ee9b231ec0bd031531be57
swamimalavade <swami.91196@gmail.com>
2024-01-02 12:03:38 +0530

    1st solve

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,20 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { CompFailureDirective } from './comp-failure.directive';
+import { CompSuccessDirective } from './comp-success.directive';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    CompFailureDirective,
+    CompSuccessDirective,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'assigment-10';
